32bfea892d22fab015ce93ae3170f3c4f45fcf15
ArtavazdGevorgyan <ag15012003@gmail.com>
2022-03-09 03:58:53 +0400

    Update errors.py

--- errors.py
+++ errors.py
@@ -4,13 +4,15 @@ def fileerror():
     print("||| FILE IS NOT DEFINED")
     sys.exit()
 
-def varname(line):
+def varname(line, index):
+    print(f"Line: {index+1} ||| {line}")
     print(f"||| {line}")
     print(" Variable name can starts with letter or underscore")
     sys.exit()
 
-def syntaxerror(line):
-    print(f"Invalid Syntax: {line}")
+def syntaxerror(line, index):
+    print(f"Line: {index+1} ||| {line}")
+    print(f"Invalid Syntax")
     sys.exit()
 
 def logicerror():
@@ -18,6 +20,14 @@ def logicerror():
     sys.exit()
 
 def undef(line, index):
-    print(f"Line: {index} ||| {line}")
+    print(f"Line: {index+1} ||| {line}")
     print(f" name '{line[0]}' is not defined")
     sys.exit()
+
+def bracketerror():
+    print(f"Some of brackets are missing")
+    sys.exit()
+
+def elseerror(line, index):
+    print(f"Line: {index+1} ||| {line}")
+    print("Ther is no if for this else")
